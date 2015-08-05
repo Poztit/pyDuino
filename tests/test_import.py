18f55938df747aff5982ab7476c9dc8f23940662
Vayel <vincent.lefoulon@free.fr>
2015-08-05 16:47:11 +0200

    Add TravisCI

---
+++ tests/test_import.py
@@ -0,0 +1,10 @@
+def test_core():
+    from pyduino.core import base 
+    from pyduino.core import common
+    from pyduino.core import libs
+    from pyduino.core import system
+
+def test_pyduino():
+    from pyduino import light
+    from pyduino import pcduino
+    from pyduino import rpi
